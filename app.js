b9d09e5cdba59f5f8591843ff3b19b3f555cfde9
Ashish jaswal <jaswalashish131@gmail.com>
2023-02-22 20:18:42 +0530

    adding md-5

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const ejs = require('ejs');
 const app = express();
 const mongoose = require('mongoose');
 const { strict } = require('assert');
-const encrypt = require('mongoose-encryption');
+// const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
+
 
 console.log(process.env.SECRET);
 
@@ -24,7 +26,7 @@ const userSchema  = new mongoose.Schema( {
 });
 
 
-userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:['password']});
+// userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:['password']});
 
 const User = new mongoose.model('User',userSchema);
 
@@ -52,7 +54,7 @@ app.get('/register',function(req,res){
 app.post('/register',function(req,res){
    const newUser = new User({
     email:req.body.username,
-    password:req.body.password
+    password:md5(req.body.password)
    });
 
    newUser.save(function(err){

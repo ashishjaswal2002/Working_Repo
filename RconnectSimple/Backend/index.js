4cd5d42246ed315888a0aedb32b0b5eb7daa339b
Ashish jaswal <jaswalashish131@gmail.com>
2023-03-02 20:15:10 +0530

    Middleware done

--- RconnectSimple/Backend/index.js
+++ RconnectSimple/Backend/index.js
@@ -1,1 +1,56 @@
-console.log("HElli");
+const express = require('express');
+const server  = express();
+const port  =  8080;
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+//Connection between mongoose 
+mongoose.set('strictQuery',true);
+
+
+
+async function main(){
+ await mongoose.connect('mongodb://localhost:27017/demo',{usenewUrlParser:true});
+ console.log('Mongodb connected');
+}
+
+main().catch(err=>console.log(err.message));
+
+//Create Schema 
+const userSchema =new mongoose.Schema({
+  username:{
+    type:String
+  },
+  password:{
+    type:String
+  }
+})
+
+const User = mongoose.model('User',userSchema); 
+
+//Adding a MiddleWare 
+
+server.use(cors());
+server.use(bodyParser.json());
+
+server.post('/demo',function(req,res){
+ let user = new User();
+   user.username = req.body.username;
+   user.password = req.body.password;
+
+   user.save();
+  console.log(req.body)
+   res.json(req.body);
+})
+
+
+server.get('/demo',async(req,res)=>{
+const docs  = await User.find({});
+res.json(docs);
+})
+
+
+
+server.listen(port,()=>{
+    console.log('Server listening on port '+port);
+})
--- RconnectSimple/Frontend/src/App.jsx
+++ RconnectSimple/Frontend/src/App.jsx
@@ -1,33 +1,77 @@
+import { useEffect } from "react";
 import { useState } from "react";
 function App() {
 
-  const [form,setform] = useState('');
+  const [form,setform] = useState({
+    username:'',
+    password:''
+  });
+  const [users,setUsers] = useState([]);
 
 const handleform = (e)=>{
- 
+  console.log("HandleForm")
+ if(!e.target.value){
+  return;
+ }
   setform({
     ...form,
     [e.target.name]: e.target.value
   });
 
   
+  
 }
-const handleSubmit = (e)=>{
+const handleSubmit = async(e)=>{
   e.preventDefault();
-  console.log(form);
-}
+//Adding Fetch method
+const response = await fetch('http://localhost:8080/demo',{
+  method:'POST',
+  body:JSON.stringify(form),
+  headers:{
+    'Content-Type':'application/json',
 
+
+  }
+
+})
+const data = await response.json();
+  
+  setform({
+    username:'',
+    password:''
+  });
+
+}
+const getUsers = async()=>{
+  const response = await fetch('http://localhost:8080/demo',{
+  method:'GET',
+})
+const data = await response.json();
+  setUsers(data);
+
+}
+useEffect(()=>{
+  getUsers();
+},[users])
+
+ 
   return (
     <div >
     <form action="" onSubmit={handleSubmit}>
   {/* {JSON.stringify(form)} For the debugging purposes* */}
 <span>Username</span>
-      <input type="text" name="username" onChange={handleform}/>
+      <input type="text" name="username" value={form.username} onChange={handleform}/>
       <span>Password</span>
-      <input type="text" name="password"  onChange={handleform}/>
+      <input type="text" name="password" value={form.password}  onChange={handleform}/>
       <input type="submit" />
     </form>
-
+<div>
+  <ul>
+    {users.map(user=>(<li key={user._id}>
+      {user.username}
+    </li>))}
+  </ul>
+</div>
     </div>
   )
 }
